dc69bad51ff8ff2858c5f052732d21c2d2f5e0aa
deymarchow <campeonyed407@gmail.com>
2017-10-26 18:45:39 -0400

    Add files via upload

---
+++ diabetes.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+
+GSM =pd.read_csv('phone_dataset.csv',header=0)
+desf=pd.DataFrame(GSM)
+
+print GSM
